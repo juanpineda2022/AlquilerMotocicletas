014e5abbd13d6ec8072d80fe241215b5a15018a1
Juan Pineda <juan.pineda1.mt@usa.edu.co>
2022-10-08 05:47:37 -0500

    Backend prueba2

---
+++ AlquilerMotocicletasProyecto/src/main/java/com/usa/ciclo3/g34/proyectoMotocicletas/repository/MotocicletaCrudRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.usa.ciclo3.g34.proyectoMotocicletas.repository;
+
+import com.usa.ciclo3.g34.proyectoMotocicletas.model.Motocicleta;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author jpine
+ */
+public interface MotocicletaCrudRepository extends CrudRepository<Motocicleta, Integer>{
+    
+}
